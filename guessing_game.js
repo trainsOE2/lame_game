db8b7ef73bb368b7dea4258d7148b8edf88827a2
Megha Singh <Megha Singh>
2016-12-26 10:44:24 +0530

    insignificant modification

--- guessing_game.js
+++ guessing_game.js
@@ -1,20 +1,17 @@
 
 
   var target;
-  var done = false;
   var input;
   var colors = ["blue", "green", "yellow", "red", "violet", "indigo", "orange", "cyan", "pink", "grey", "black", "white", "maroon", "brown"];
   colors.sort();
   var index = 0;
-  var guesses = 0;
   var done = false;
-  var i;
+  var count;
   var valid = false;
 
 
   function game()
   {
-    input = prompt("What color am I thinking of?\n" + colors + "\nEnter your guess\n");
     index = Math.floor(Math.random()*14);
     target = colors[index];
 
@@ -54,16 +51,22 @@
 
     check_validity()
     {
-      i = 0;
-      if(input != colors[i])
+      count = 0;
+
+      for( present_color of colors)
       {
-        i++;
+        if(input != present_color)
+        {
+          count++;
+        }
       }
-      if(i<14)
+
+      if(count < 14)
       {
         return true;
       }
-      if(i => 14)
+
+      if(count = 14)
       {
         return false;
       }
